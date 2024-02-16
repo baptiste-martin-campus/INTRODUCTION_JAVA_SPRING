bdab004ed279326a8706e89c1c79f455b1b9fb91
Baptiste Martin <baptiste.martin@le-campus-numerique.fr>
2024-02-16 10:14:03 +0100

    initial commit

---
+++ src/main/java/org/campusnumerique/introjavaspring/IntroJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package org.campusnumerique.introjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/campusnumerique/introjavaspring/IntroJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.campusnumerique.introjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
